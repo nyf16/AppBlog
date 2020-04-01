962c00471f3d1a062e44aad51b452555a0db7aba
Onur FERIK <49190459+nyf16@users.noreply.github.com>
2020-04-02 02:25:49 +0300

    User signIn

--- BlogApp/Controllers/AuthController.cs
+++ BlogApp/Controllers/AuthController.cs
@@ -12,10 +12,13 @@ namespace BlogApp.Controllers
     public class AuthController : Controller
     {
         private SignInManager<IdentityUser> _signInManager;
+        private UserManager<IdentityUser> _userManager;
 
-        public AuthController (SignInManager<IdentityUser> signInManager)
+        public AuthController(SignInManager<IdentityUser> signInManager,
+            UserManager<IdentityUser> userManager)
         {
             _signInManager = signInManager;
+            _userManager = userManager;
         }
 
         [HttpGet]
@@ -28,7 +31,54 @@ namespace BlogApp.Controllers
         public async Task<IActionResult> Login(LoginViewModel vm)
         {
             var result = await _signInManager.PasswordSignInAsync(vm.UserName, vm.Password, false, false);
-            return RedirectToAction("Index", "Panel");
+
+            if (!result.Succeeded)
+            {
+                return View(vm);
+            }
+
+            var user = await _userManager.FindByNameAsync(vm.UserName);
+
+            var isAdmin = await _userManager.IsInRoleAsync(user, "Admin");
+
+            if (isAdmin)
+            {
+                return RedirectToAction("Index", "Panel");
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View(new RegisterViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel vm)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(vm);
+            }
+
+            var user = new IdentityUser
+            {
+                UserName = vm.Email,
+                Email = vm.Email
+            };
+
+            var result = await _userManager.CreateAsync(user, "password");
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(vm);
         }
 
         [HttpGet]
--- BlogApp/Data/Repository/Repository.cs
+++ BlogApp/Data/Repository/Repository.cs
@@ -1,4 +1,5 @@
-﻿using BlogApp.Models;
+﻿using BlogApp.Helpers;
+using BlogApp.Models;
 using BlogApp.Models.Comments;
 using BlogApp.ViewModels;
 using Microsoft.EntityFrameworkCore;
@@ -35,7 +36,7 @@ namespace BlogApp.Data.Repository
         {
             Func<Post, bool> InCategory = (post) => { return post.Category.ToLower().Equals(category.ToLower()); };
 
-            int pageSize = 1;
+            int pageSize = 2;
             int skipAmount = pageSize * (pageNumber - 1);
 
             var query = _ctx.Posts.AsQueryable();
@@ -51,7 +52,7 @@ namespace BlogApp.Data.Repository
                 PageNumber = pageNumber,
                 PageCount = pageCount,
                 NextPage = postsCount > skipAmount + pageSize,
-                Pages = PageNumbers(pageNumber, pageCount),
+                Pages = PageHelper.PageNumbers(pageNumber, pageCount).ToList(),
                 Category = category,
                 Posts = query
                      .Skip(skipAmount)
@@ -62,40 +63,7 @@ namespace BlogApp.Data.Repository
 
         }
 
-        private IEnumerable<int> PageNumbers(int pageNumber, int pageCount)
-        {            
-            int midPoint = pageNumber < 3 ? 3
-                : pageNumber > pageCount - 2 ? pageCount - 2
-                : pageNumber;
-
-            int lowerBound = midPoint - 2;
-            int upperBound = midPoint + 2;
-
-            if (lowerBound != 1)
-            {
-                yield return 1;
-                if (lowerBound - 1 > 1)
-                {
-                    yield return -1;
-                }
-
-            }
-
-            for (int i = midPoint - 2; i <= midPoint + 2; i++)
-            {
-                yield return i;
-            }
-
-            if (upperBound != pageCount)
-            {
-                if (pageCount - upperBound > 1)
-                {
-                    yield return -1;
-                }
-                yield return pageCount;
-            }
-            
-        }
+        
         public Post GetPost(int id)
         {
             return _ctx.Posts
